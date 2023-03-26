46e28f999686870aa5970c7c83a9afcbaad587a1
Rafał Opiłowski <rafalopilowski1@outlook.com>
2023-03-26 22:16:34 +0200

    Finish day 2023-03-22

--- 2023_03_22/zad04.py
+++ 2023_03_22/zad04.py
@@ -3,11 +3,10 @@ Lab03 - zadanie 04
 """
 
 limit = int(input("Podaj liczbę graniczną: "))
-
 catalanNumber = 1
 print(catalanNumber)
-for n in range(0, limit + 1):
-    catalanNumber *= 2 * (2 * n + 1) / (n + 2)
-    if catalanNumber > limit:
-        break
-    print(int(catalanNumber))
+n = 0
+while catalanNumber <= limit:
+    catalanNumber *= 2 * (2 * n + 1) // (n + 2)
+    print(catalanNumber)
+    n += 1
--- 2023_03_22/zad05.py
+++ 2023_03_22/zad05.py
@@ -2,17 +2,35 @@
 Lab03 - zadanie 05
 """
 
-db = []
+db = dict()
 choice = -1
 while choice != 0:
     choice = int(input("Podaj wybór: "))
-    if choice == 1:
-        number = int(input("Podaj liczbę do dodania: "))
-        db.append(number)
-    if choice == 2:
-        number = int(input("Podaj liczbę do usunięcia: "))
-        db.remove(number)
-    if choice == 3:
-        numbers = [int(x) for x in input("Podaj liczby do zamiany: ").split(",")]
-
-        db.remove(number)
+    if choice == 0:
+        print("Do zobaczenia!")
+    if choice == 1:  # Dodawanie
+        name = input("Podaj nazwę produktu: ")
+        amount = int(input("Podaj ilość: "))
+        price = float(input("Podaj cenę: "))
+        item = (name, amount, price)
+        id = len(db) + 1
+        db[id] = item
+        print(f"Dodano produkt: {id}: {item}")
+        print("Gotowe!")
+    if choice == 2:  # Usuwanie
+        number = int(input("Podaj numer produktu do usunięcia: "))
+        db.pop(number)
+        print("Gotowe!")
+    if choice == 3:  # Modyfikacja
+        number = int(input("Podaj numer produktu do modyfikacji: "))
+        (name, amount, price) = db[number]
+        db.pop(number)
+        newName = input(f"Nazwa: {name} --> ")
+        newAmount = int(input(f"Ilość: {amount} --> "))
+        newPrice = float(input(f"Cena: {price} --> "))
+        db[number] = (newName, newAmount, newPrice)
+        print("Gotowe!")
+    if choice == 4:
+        number = int(input("Podaj liczbę do wyświetlenia: "))
+        print(db[number])
+        print("Gotowe!")
